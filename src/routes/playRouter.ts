6f89caa6c58f822abc57dc50c0bccd514dbb40d2
mnebot86 <miguelnbt@gmail.com>
2023-12-10 20:54:27 -0500

    Formation / Plays Controllers
    ## Whats this for?
    Update controllers for formation. Update this shape of the return data. Emit new plays
    
    ## Changes
    - removed image size config
    
    ## Testing
    - What able to successfully add a play to formation.

--- src/routes/playRouter.ts
+++ src/routes/playRouter.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import * as PlayController from '../controllers/playController';
 import { requireAuth } from '../middleware/checkAuth';
 import { setupMulter } from '../utils/muter';
-
+import multer from 'multer';
 const router = express.Router({ mergeParams: true });
 
 const uploads = setupMulter();
 
-router.route('/').post(uploads.single('play_picture'), requireAuth, PlayController.createPlay).get(requireAuth, PlayController.getAllPlays);
-router.route('/:id').get(requireAuth, PlayController.getPlay).patch(uploads.single('play_picture'), requireAuth, PlayController.updatePlay).delete(requireAuth, PlayController.deletePlay);
+router.route('/').post(uploads.single('play_image'), requireAuth, PlayController.createPlay).get(requireAuth, PlayController.getAllPlays);
+router.route('/:id').get(requireAuth, PlayController.getPlay).patch(uploads.single('play_image'), requireAuth, PlayController.updatePlay).delete(requireAuth, PlayController.deletePlay);
 
 export default router;
