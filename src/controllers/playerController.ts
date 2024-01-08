da0b1771da4ab43b6b84bbf47f2fe6ee74eeca30
mnebot86 <miguelnbt@gmail.com>
2024-01-07 20:57:31 -0500

    [Feature] - Update Player
    ## Whats this for?
    Api for updating a player information and avatar
    
    ## Changes
    - new controller updatePlayer

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -1,9 +1,13 @@
+import { v2 as cloudinary } from "cloudinary";
 import { RequestHandler } from "express";
-import Player from "../models/player";
-import User from "../models/user";
 import createHttpError from "http-errors";
 import { StatusCodes } from "http-status-codes";
 import { io } from "../app";
+import Player from "../models/player";
+import User from "../models/user";
+import { initializeCloudinary } from "../utils/cloudinary";
+
+initializeCloudinary();
 
 export const createPlayer: RequestHandler = async (req, res, next) => {
   const { userId } = req.session;
@@ -39,6 +43,38 @@ export const createPlayer: RequestHandler = async (req, res, next) => {
     res.status(StatusCodes.CREATED).json(player.toObject());
   } catch (error) {
     next(error);
+  }
+};
+
+export const updatePlayer: RequestHandler = async (req, res, next) => {
+  const { imageId } = req.body;
+  
+  try {
+    const player = await Player.findByIdAndUpdate(req.params.playerId, req.body, { new: true });
+
+    if (!player) { 
+      return res.status(StatusCodes.NOT_FOUND).json({ message: "Player not found." });
+    }
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        public_id: `avatar/${imageId}`,
+        crop: "fill",
+      });
+
+      player.avatar = {
+        url: result.secure_url,
+        imageId: imageId,
+      };
+
+      await player.save();
+    }
+
+    io.emit("update_player");
+
+    return res.status(StatusCodes.OK).json(player);
+  } catch (error) {
+    next(error)
   }
 };
 
--- src/routes/playerRouter.ts
+++ src/routes/playerRouter.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as PlayerController from '../controllers/playerController';
 import { requireAuth } from '../middleware/checkAuth';
+import { setupMulter } from "../utils/muter";
+
+const uploads = setupMulter();
 
 const router = express.Router();
 
 router.route('/').post(requireAuth, PlayerController.createPlayer).get(requireAuth, PlayerController.getPlayers);
-router.route('/:playerId').get(requireAuth, PlayerController.getPlayer);
+router.route('/:playerId').get(requireAuth, PlayerController.getPlayer).patch(uploads.single("avatar"), requireAuth, PlayerController.updatePlayer);
 
 export default router;
